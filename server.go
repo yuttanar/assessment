7c1482778fad39530a363cd87ffcb995a400fbb9
Yuttanar <yuttanar.tery@gmail.com>
2023-01-04 23:59:43 +0700

    add api port get from environment variable name PORT

--- server.go
+++ server.go
@@ -2,19 +2,17 @@ package main
 
 import (
 	"fmt"
-	"os"
-
-	"net/http"
-
 	"github.com/labstack/echo/v4"
+	"net/http"
+	"os"
 )
 
 func main() {
-	fmt.Println("Please use server.go for main file")
-	fmt.Println("start at port:", os.Getenv("PORT"))
 	e := echo.New()
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!")
 	})
-	e.Logger.Fatal(e.Start(":1323"))
+	e.Logger.Fatal(e.Start(":" + os.Getenv("PORT")))
+	fmt.Println("Please use server.go for main file")
+	fmt.Println("start at port:", os.Getenv("PORT"))
 }
