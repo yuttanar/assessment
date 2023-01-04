de7a2a742a2fd897ccdc0b54e892f74ca444bf66
Yuttanar <yuttanar.tery@gmail.com>
2023-01-04 23:54:41 +0700

    add echo framework

--- server.go
+++ server.go
@@ -3,9 +3,18 @@ package main
 import (
 	"fmt"
 	"os"
+
+	"net/http"
+
+	"github.com/labstack/echo/v4"
 )
 
 func main() {
 	fmt.Println("Please use server.go for main file")
 	fmt.Println("start at port:", os.Getenv("PORT"))
+	e := echo.New()
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, World!")
+	})
+	e.Logger.Fatal(e.Start(":1323"))
 }
