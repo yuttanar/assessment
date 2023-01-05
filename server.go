66fe37c6ec144cd254674c4f36fc2af57a1111a0
Yuttanar <yuttanar.tery@gmail.com>
2023-01-05 17:24:43 +0700

    add graceful shutdown & set default ENV PORT

--- server.go
+++ server.go
@@ -1,18 +1,47 @@
 package main
 
 import (
+	"context"
 	"fmt"
-	"github.com/labstack/echo/v4"
 	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/labstack/echo/v4"
 )
 
 func main() {
+	var PORT string = os.Getenv("PORT")
+	if PORT == "" {
+		PORT = ":2565"
+	}
+
+	fmt.Println("Please use server.go for main file")
+	fmt.Println("start at port:", PORT)
+	
+
 	e := echo.New()
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!")
 	})
-	e.Logger.Fatal(e.Start(":" + os.Getenv("PORT")))
-	fmt.Println("Please use server.go for main file")
-	fmt.Println("start at port:", os.Getenv("PORT"))
+
+	// Start go server
+	go func() {
+		if err := e.Start(PORT); err != nil && err != http.ErrServerClosed {
+			e.Logger.Fatal("shutting down the server")
+		}
+	}()
+
+	shutdown := make(chan os.Signal, 1)
+	signal.Notify(shutdown, os.Interrupt, syscall.SIGTERM)
+	<-shutdown
+	fmt.Println("shutting down the server, bye...")
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	if err := e.Shutdown(ctx); err != nil {
+		e.Logger.Fatal(err)
+	}
+
 }
